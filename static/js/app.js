f276657a19f12f56858fc68a3eff9248f88667e1
Ju A Han <jane.han13@gmail.com>
2020-09-01 17:18:28 -0600

    Added recommendation

--- static/js/app.js
+++ static/js/app.js
@@ -7,18 +7,18 @@ function runEnter(event) {
     event.preventDefault();
 
     var freetimeEl = d3.select("#freetime")
-    var ageEl = d3.select("#age")
+    var studytimeEl = d3.select("#studytime")
     var healthEl = d3.select('#health')
     var WalcEl = d3.select("#Walc")
     var gooutEl = d3.select('#goout')
 
     var freetime = freetimeEl.property("value")
-    var age = ageEl.property("value")
+    var studytime = studytimeEl.property("value")
     var health = healthEl.property("value")
     var Walc = WalcEl.property("value")
     var goout = gooutEl.property("value")
 
-    var responses = [freetime, age, health, Walc, goout]
+    var responses = [freetime, studytime, health, Walc, goout]
 
     // console.log(responses);
     
@@ -56,6 +56,24 @@ function runEnter(event) {
         Plotly.newPlot('gauge', data, layout);
         
         d3.select("#recommendation").html('')
-        d3.select("#recommendation").html(`<p><span>The predicted final grade level is ${response.result}.</span></p>`);
+        d3.select("#recommendation").html(`<p><span>4 is the highest and 1 is the lowest grade<br>
+        Your predicted final grade level is ${response.result}<br><br>
+        
+        Suggestions for getting a higher grade:<br>
+        Studytime: Moderate to High<br>
+        Health: Moderate <br>
+        Freetime: Moderate to Low <br>
+        Going out with friends: Moderate to Low <br>
+        Weekend Alcohol Consumption: Low <br>
+        Weekday Alcohol Consumption: Very Low <br>
+        Absences: Less than 4 <br>
+        Past Class Failures: 1-0 <br>
+        <br>
+        To end with a grade in the top 90th Percentile <br>
+        First Trimester Grade: 16 or Higher <br>
+        Second Trimester Grade: 18 or Higher <br>
+        <br>
+        These suggestions were made by analyzing the data attribute averages for the top 90th performance percentile.
+        </span></p>`);
     })
 }
